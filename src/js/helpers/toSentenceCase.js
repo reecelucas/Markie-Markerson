347910692f0d0dc4c5c1c2f11bc2c8d81469b432
Reece Lucas <reece.lucas@bbc.co.uk>
2018-12-18 09:58:28 +0000

    Sentence casing bug fixed

--- src/js/helpers/toSentenceCase.js
+++ src/js/helpers/toSentenceCase.js
@@ -6,16 +6,12 @@
  *
  * Taken from: https://tinyurl.com/y8wlmf9g
  *
- * TODO: this has been crudely adapted to work when a full-stop
- * proceeds a <br> tag. Really this should be incorporated into the
- * existing regex.
+ * Note: this will only correctly format sentences if they end with
+ * one of the following: `.` `!` or `?`.
  */
 
 export default comment =>
-  comment.replace(/.+?[.?!](\s|$)/g, text => {
-    const casedString = `${text.charAt(0).toUpperCase()}${text.substr(1).toLowerCase()}`;
-    return casedString
-      .split('.<br>')
-      .map(segment => `${segment.charAt(0).toUpperCase()}${segment.substr(1).toLowerCase()}`)
-      .join('');
-  });
+  comment.replace(
+    /.*?(([.!?]\s?<br\s*\/?>)|[.!?])(\s?|$)/g,
+    text => `${text.charAt(0).toUpperCase()}${text.substr(1).toLowerCase()}`
+  );
